26b05d22dccd85e6f5cad17b9b34d90677c7f28f
swedev99 <swedevops99@gmail.com>
2023-05-02 11:17:38 +0530

    terraform sample aws instance

--- awsinstancesall.tf
+++ awsinstancesall.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "centos" {
+resource "aws_instance" "fronend" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -6,7 +6,7 @@ resource "aws_instance" "centos" {
     Name = "frontend"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "mongodb" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -14,7 +14,7 @@ resource "aws_instance" "centos" {
     Name = "mongodb"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "catalogue" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -22,7 +22,7 @@ resource "aws_instance" "centos" {
     Name = "catalogue"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "redis" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -30,7 +30,7 @@ resource "aws_instance" "centos" {
     Name = "redis"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "user" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -38,7 +38,7 @@ resource "aws_instance" "centos" {
     Name = "user"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "cart" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -46,7 +46,7 @@ resource "aws_instance" "centos" {
     Name = "cart"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "mysql" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -54,7 +54,7 @@ resource "aws_instance" "centos" {
     Name = "mysql"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "shipping" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
@@ -62,14 +62,14 @@ resource "aws_instance" "centos" {
     Name = "shipping"
   }
 }
-resource "aws_instance" "centos" {
+resource "aws_instance" "rabbitmq" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
   tags = {
     Name = "rabbitmq"
   }
-}resource "aws_instance" "centos" {
+}resource "aws_instance" "payment" {
   ami           = "ami-0b5a2b5b8f2be4ec2"
   instance_type = "t3.micro"
 
