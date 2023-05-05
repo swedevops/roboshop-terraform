fb9f22b7164dea564521d02252f8bb0dbe3024de
swedev99 <swedevops99@gmail.com>
2023-05-05 19:42:27 +0530

    code dry

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -42,3 +42,4 @@ default = {
 }
 
 env = "dev"
+
--- servers.tf
+++ servers.tf
@@ -9,13 +9,14 @@ resource "aws_instance" "instance" {
   }
 }
 resource "aws_route53_record" "records" {
-  for_each  =var.components
+  for_each  = var.components
   zone_id = "Z0587270PBVKKHW0FPNL"
   name    = "${each.value["name"]}-dev.swedev99.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
 }
+
 
 
 
