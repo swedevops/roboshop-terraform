6b14e50d8fac73b9ceb2bcc8257bc928ff2127b9
swedev99 <swedevops99@gmail.com>
2023-05-07 15:23:11 +0530

    code dry

---
+++ module/main.tf
@@ -0,0 +1,40 @@
+resource "aws_instance" "instance" {
+  ami                    = data.aws_ami.centos.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [data.aws_security_group.test.id]
+
+  tags = {
+    Name = var.component_name
+  }
+}
+resource "null_resource" "provisioner" {
+  depends_on = [aws_instance.instance, aws_route53_record.records]
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/swedevops/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
+}
+resource "aws_route53_record" "records" {
+  zone_id  = "Z0587270PBVKKHW0FPNL"
+  name     = "${var.component_name}-dev.swedev99.online"
+  type     = "A"
+  ttl      = 30
+  records  = [aws_instance.instance.private_ip]
+}
+
+
+
+
+
+
--- servers.tf
+++ servers.tf
@@ -1,43 +1,9 @@
-resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.test.id]
-
-  tags = {
-    Name = each.value["name"]
-  }
-}
-  resource "null_resource" "provisioner" {
-    depends_on = [aws_instance.instance, aws_route53_record.records]
+module "servers" {
   for_each = var.components
-    provisioner "remote-exec" {
 
-      connection {
-    type     = "ssh"
-    user     = "centos"
-    password = "DevOps321"
-    host     = aws_instance.instance[each.value["name"]].private_ip
-  }
-  inline = [
-    "rm -rf roboshop-shell",
-    "git clone https://github.com/swedevops/roboshop-shell.git",
-    "cd roboshop-shell",
-    "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password","null")}"
-  ]
+  source = "./module"
+  component_name = each.value ["name"]
+  env = var.env
+  instance_type = each.value["instance_type"]
+  password = lookup(each.value,"password", "null")
 }
-}
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = "Z0587270PBVKKHW0FPNL"
-  name     = "${each.value["name"]}-dev.swedev99.online"
-  type     = "A"
-  ttl      = 30
-  records  = [aws_instance.instance[each.value["name"]].private_ip]
-}
-
-
-
-
-
-
