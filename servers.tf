1145db64333e96cbb498e4dc9b94692f4fcf35d1
swedev99 <swedevops99@gmail.com>
2023-05-09 14:40:15 +0530

    code dry

--- servers.tf
+++ servers.tf
@@ -1,5 +1,16 @@
-module "servers" {
-  for_each       =  var.components
+module "database_servers" {
+  for_each       =  var.database_servers
+
+  source         =  "./module"
+  component_name =  each.value ["name"]
+  env            =  var.env
+  instance_type  =  each.value["instance_type"]
+  password       =  lookup(each.value, "password", "null")
+  provisioner    = true
+}
+module "app_servers" {
+  depends_on = [module.database_servers]
+  for_each       =  var.app_servers
 
   source         =  "./module"
   component_name =  each.value ["name"]
--- vars.tf
+++ vars.tf
@@ -1,2 +1,6 @@
 variable "env" { }
-variable "components" {}
+variable "app_servers" {}
+variable "database_servers" {}
+
+
+
