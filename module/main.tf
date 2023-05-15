6a298fda264f3000c95eafd026f62dd2c96db953
swedev99 <swedevops99@gmail.com>
2023-05-15 20:53:16 +0530

    code dry

--- module/main.tf
+++ module/main.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.test.id]
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   tags = {
     Name = local.name
@@ -60,10 +61,16 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component_name}-${var.env}-role"
+  role = aws_iam_role.role.name
+}
 
 resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
+
+
 
   policy = jsonencode({
     "Version": "2012-10-17",
