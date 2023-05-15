006295aa657ae5ef5a448072408571c01a376f81
swedev99 <swedevops99@gmail.com>
2023-05-15 19:39:29 +0530

    code dry

--- module/main.tf
+++ module/main.tf
@@ -61,6 +61,36 @@ resource "aws_iam_role" "role" {
 }
 
 
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+  {
+    "Sid": "VisualEditor0",
+    "Effect": "Allow",
+    "Action": [
+    "ssm:GetParameterHistory",
+    "ssm:GetParametersByPath",
+    "ssm:GetParameters",
+    "ssm:GetParameter"
+  ],
+    "Resource": "arn:aws:ssm:us-east-1:135820335997:parameter/${var.component_name}-${var.env}.*"
+  },
+  {
+    "Sid": "VisualEditor1",
+    "Effect": "Allow",
+    "Action": "ssm:DescribeParameters",
+    "Resource": "*"
+  }
+  ]
+  })
+  }
+
+
+
 
 
 
