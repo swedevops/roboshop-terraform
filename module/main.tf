654fffdea9fac76844cc852874ca0e0311cc51dd
swedev99 <swedevops99@gmail.com>
2023-05-17 17:36:15 +0530

    code dry

--- module/main.tf
+++ module/main.tf
@@ -11,6 +11,7 @@ resource "aws_instance" "instance" {
 }
 resource "null_resource" "provisioner"  {
   depends_on = [aws_instance.instance, aws_route53_record.records]
+
   triggers = {
     private_ip = aws_instance.instance.private_ip
   }
