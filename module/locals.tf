db65da29b4171199e0790ce78f8c0c4bc3db2fd3
swedev99 <swedevops99@gmail.com>
2023-05-19 15:54:13 +0530

    code dry

--- module/locals.tf
+++ module/locals.tf
@@ -2,13 +2,13 @@ locals {
   name        = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
     "rm -rf roboshop-shell",
-    "git clone https://github.com/raghudevopsb72/roboshop-shell",
+    "git clone https://github.com/swedevops/roboshop-shell.git",
     "cd roboshop-shell",
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
   app_commands = [
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/swedevops/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/swedevops/roboshop-shell roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
 
