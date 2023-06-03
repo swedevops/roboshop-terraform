cec04f3fc5997f1943f4be0df25d897574d4b13d
swedev99 <swedevops99@gmail.com>
2023-06-03 11:43:19 +0530

    code dry

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ module "vpc" {
 
 module "web" {
   source = "git::https://github.com/swedevops/tf-module-app.git"
+
   for_each = var.app
   instance_type = each.value["instance_type"]
   subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
