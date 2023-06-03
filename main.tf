15ca3117561564d0b0d17a701aca84007f7b8d70
swedev99 <swedevops99@gmail.com>
2023-06-04 01:02:34 +0530

    code dry

--- main.tf
+++ main.tf
@@ -1,31 +1,31 @@
 module "vpc" {
-  source = "git::https://github.com/swedevops/tf-module-vpc.git"
-  for_each = var.vpc
-  cidr_block  = each.value["cidr_block"]
-  subnets = each.value["subnets"]
-  tags = local.tags
-  env = var.env
+  source       = "git::https://github.com/swedevops/tf-module-vpc.git"
+  for_each     = var.vpc
+  cidr_block   = each.value["cidr_block"]
+  subnets      = each.value["subnets"]
+  tags         = local.tags
+  env          = var.env
 }
 
 module "app" {
-  source = "git::https://github.com/swedevops/tf-module-app.git"
-
-  for_each = var.app
-  instance_type = each.value["instance_type"]
-  name = each.value["name"]
+  source           = "git::https://github.com/swedevops/tf-module-app.git"
+  for_each         = var.app
+  instance_type    = each.value["instance_type"]
+  name             = each.value["name"]
   desired_capacity = each.value["desired_capacity"]
-  max_size = each.value["max_size"]
-  min_size = each.value["min_size"]
+  max_size         = each.value["max_size"]
+  min_size         = each.value["min_size"]
 
 
-  env = var.env
-  bastion_cidr = var.bastion_cidr
+  env            = var.env
+  bastion_cidr   = var.bastion_cidr
 
-  subnet_ids = lookup(lookup(lookup(lookup(module.vpc, "main" , null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
-  vpc_id = lookup(lookup(module.vpc, "main" , null), "vpc_id",null)
-  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main" , null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
+  allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
+
 }
-
 
 
 
