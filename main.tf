51e54dfb4f29c4ed5208bab5eeb470b1757c9eca
swedev99 <swedevops99@gmail.com>
2023-06-08 14:15:05 +0530

    code dry

--- main.tf
+++ main.tf
@@ -103,10 +103,9 @@ module "app" {
   min_size         = each.value["min_size"]
   app_port         = each.value["app_port"]
   listener_priority = each.value["listener_priority"]
-  dns_name          = lookup(var.app, "dns_name", "${each.value["name"]}-${var.env}")
+  dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
 
-
-subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   vpc_id         = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
   listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
