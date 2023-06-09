fef8f63f9cd9994d813266b3bdce8fc78ffd90ae
swedev99 <swedevops99@gmail.com>
2023-06-09 13:23:21 +0530

    code dry

--- main.tf
+++ main.tf
@@ -93,15 +93,16 @@ module "alb" {
 }
 
 module "app" {
-  depends_on = [module.vpc, module.docdb, module.rds,module.elasticache,module.elasticache,module.alb ]
-  source           = "git::https://github.com/swedevops/tf-module-app.git"
-  for_each         = var.app
-  instance_type    = each.value["instance_type"]
-  name             = each.value["name"]
-  desired_capacity = each.value["desired_capacity"]
-  max_size         = each.value["max_size"]
-  min_size         = each.value["min_size"]
-  app_port         = each.value["app_port"]
+  depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.rabbitmq, module.alb]
+  source     = "git::https://github.com/swedevops/tf-module-app.git"
+
+  for_each          = var.app
+  instance_type     = each.value["instance_type"]
+  name              = each.value["name"]
+  desired_capacity  = each.value["desired_capacity"]
+  max_size          = each.value["max_size"]
+  min_size          = each.value["min_size"]
+  app_port          = each.value["app_port"]
   listener_priority = each.value["listener_priority"]
   dns_name          = each.value["name"] == "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
 
@@ -110,14 +111,14 @@ module "app" {
   allow_app_cidr = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_app_cidr"], null), "subnet_cidrs", null)
   listener_arn   = lookup(lookup(module.alb, each.value["lb_type"], null), "listener_arn", null)
   lb_dns_name    = lookup(lookup(module.alb, each.value["lb_type"], null), "dns_name", null)
-  env            = var.env
-  bastion_cidr   = var.bastion_cidr
-  domain_name = var.domain_name
-  domain_id = var.domain_id
-  kms_arn = var.kms_arn
-  tags         = local.tags
-}
 
+  env          = var.env
+  bastion_cidr = var.bastion_cidr
+  tags         = local.tags
+  domain_name  = var.domain_name
+  domain_id    = var.domain_id
+  kms_arn      = var.kms_arn
+}
 
 
 
