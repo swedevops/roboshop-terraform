c8480f3fdcce1adb6f244694797c951a6935e332
swedev99 <swedevops99@gmail.com>
2023-06-22 12:36:06 +0530

    Jenkins pipeline concepts

--- main.tf
+++ main.tf
@@ -122,11 +122,39 @@ module "app" {
   monitor_cidr = var.monitor_cidr
 }
 
+#Load Runner
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "load" {
+  ami = data.aws_ami.ami.id
+  instance_type = "t3.medium"
+  vpc_security_group_ids = ["sg-0c4b5e23982e73696"]
+  tags = {
+    Name = "load-runner"
+  }
+}
+
+resource "null_resource" "load" {
+  provisioner "remote-exec" {
+
+    connection {
+      host     = aws_instance.load.private_ip
+      user     = "root"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "curl -s https://raw.githubusercontent.com/linuxautomations/labautomation/master/tools/docker/install.sh | bash",
+      "docker pull robotshop/rs-load"
+    ]
 
 
-
-
-
+  }
+}
 
 
 
